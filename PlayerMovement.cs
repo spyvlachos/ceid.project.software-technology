e8807c1fb6f8d86a2fb58275703c660cd3aac962
spyvlachos <81364057+spyvlachos@users.noreply.github.com>
2022-05-31 22:52:23 +0300

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    int jumpcount=1;
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -12,20 +12,28 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            jumpcount = 1;
+        }
+    }
     // Update is called once per frame
     private void Update()
     {
         float dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
-            {
+        if (Input.GetButtonDown("Jump") && jumpcount==1)
+        {
             rb.velocity = new Vector2(rb.velocity.x, 14);
-            }
-         if(rb.position.y < -15f)
+            jumpcount = 0;
+        }
+
+        if(rb.position.y < -15f)
         {
             FindObjectOfType<game_manager>().EndGame();
-        }    
+        }
     }
 }
