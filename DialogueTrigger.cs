ec6174b219dd9512132ca21e6205d1456fd97b17
markodim7 <97806590+markodim7@users.noreply.github.com>
2022-06-01 17:05:39 +0300

    Create DialogueTrigger.cs

---
+++ DialogueTrigger.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+
+{
+    [Header("DialoguePartner Icon")]
+    [SerializeField]private GameObject dialoguePartnerIcon;
+
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    private bool maincharInRange;
+
+    private void Awake()
+    {
+        maincharInRange = false;
+        dialoguePartnerIcon.SetActive(false);
+    }
+    
+    private  void Update()
+    {
+        if (maincharInRange)
+        {
+            dialoguePartnerIcon.SetActive(true);
+                        
+        }
+        else 
+        {
+            dialoguePartnerIcon.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            maincharInRange = true;
+        }
+
+    }
+    
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            maincharInRange = false;
+        }
+
+        
+    }
+}
