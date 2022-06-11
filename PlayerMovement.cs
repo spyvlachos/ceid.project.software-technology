80b255c51674b7766fb92bcf19a917c618664adb
chrisfrg6 <81778139+chrisfrg6@users.noreply.github.com>
2022-06-11 16:25:44 +0300

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,23 +1,22 @@
-﻿using System.Collections;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private SpriteRenderer sprite;
-    int jumpcount = 1;
+    int jumpcount=1;
     private Rigidbody2D rb;
-
-    private float direction = 0;
-    [SerializeField] private float moveSpeed = 7f;
-    [SerializeField] private float jumpForce = 14f;
-
+    public int charhealth = 5;
+    private Transform player1;
+    public GameObject magic;
+    public GameObject Arrow;
+    // public List<> hearts; 
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -25,30 +24,67 @@ public class PlayerMovement : MonoBehaviour
         {
             jumpcount = 1;
         }
+        if(collision.gameObject.tag == "enemy")
+        {
+            losehealth();
+
+        }
     }
     // Update is called once per frame
     private void Update()
     {
-        direction = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
-        if (direction > 0f)
-        {
-            sprite.flipX = false;
-        }
-        else if (direction < 0f)
-        {
-            sprite.flipX = true;
-        }
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 7, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && jumpcount == 1)
+        if (Input.GetButtonDown("Jump") && jumpcount==1)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(rb.velocity.x, 14);
             jumpcount = 0;
         }
 
-        if (rb.position.y < -15f)
+        if(rb.position.y < -15f)
         {
             FindObjectOfType<game_manager>().EndGame();
         }
+        if (charhealth<= 0)
+        {
+            Destroy(gameObject);
+            FindObjectOfType<game_manager>().EndGame();
+
+        }
+        if (Input.GetKeyDown("m"))
+        {
+            CreateMagic();
+        }
+        if (Input.GetKeyDown("j"))
+        {
+            CreateArrow();
+        }
+
+        /*  if(charhealth < 1)
+          {
+              Destroy(hearts[0].gameobject);
+          }
+          if (charhealth < 2)
+          {
+              Destroy(hearts[1].gameobject);
+          }
+          if (charhealth < 3)
+          {
+              Destroy(hearts[2].gameobject);
+          }
+          if (charhealth < 4)
+          {
+              Destroy(hearts[3].gameobject);
+          }
+          if (charhealth < 5)
+          {
+              Destroy(hearts[4].gameobject);
+          }*/
+
     }
-}
+    public void losehealth()
+    {
+        charhealth = charhealth - 1;
+        rb.velocity *= -1;
+    }
