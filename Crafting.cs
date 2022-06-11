b23a6c6c4a285fe0fea7c887993a81490b7eccc0
ariichr <101893834+ariichr@users.noreply.github.com>
2022-06-11 17:36:39 +0300

    Add files via upload

---
+++ Crafting.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafting : MonoBehaviour
+{
+    private string crafttableitems[];
+    private string resourceRequirements[];
+
+    private void craft(string item){}
+    private void createItem(string item, string performance[]){}
+    private void changeContents(string item){}
+    
+}
---
+++ CraftingMinigame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingMinigame : MonoBehaviour
+{
+    private string type;
+    private string performance[];
+
+    
+    
+}
